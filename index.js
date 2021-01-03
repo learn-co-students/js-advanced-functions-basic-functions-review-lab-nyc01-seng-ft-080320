5d91c82457107aa9cb840d4c495a124e51cb7760
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-01-03 16:14:10 -0500

    practice complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function mondayWork(action = "go to the office") {
+  return `This Monday, I will ${action}.`;
+};
+mondayWork();
+
+let wrapAdjective = function (str = "*") {
+  return function (adj = "special") {
+    return `You are ${str}${adj}${str}!`;
+  };
+};
+
+const Calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+};
+
+function actionApplyer(start, arr) {
+  let a = start;
+  for (let i = 0; i < arr.length; i++) {
+    a = arr[i](a);
+  }
+  return a;
+}
