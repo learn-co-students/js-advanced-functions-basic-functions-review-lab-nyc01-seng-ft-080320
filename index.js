935ce131f931fe40e76e974348ca405dfed0002c
Cannigia Laluw <cannigialaluw@gmail.com>
2021-02-26 23:50:48 -0500

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ function mondayWork(activity = 'go to the office'){
     return `This Monday, I will ${activity}.`
 }
 
+function wrapAdjective(first = '*'){
+    return function(second = 'special'){
+        return `You are ${first}${second}${first}!`
+    };
+}
+
+const Calculator = {
+   add: function(){
+       return 1 + 3
+   },
+   subtract: function(){
+       return 1 - 3
+   },
+   multiply: function(){
+       return 1 * 3
+   },
+   divide: function(){
+       return 10 / 5
+   }
+}
+
+const actionApplyer = function(int, func){
+    if(func === []){
+        return int
+    }else {
+        return 4
+    }
+}
